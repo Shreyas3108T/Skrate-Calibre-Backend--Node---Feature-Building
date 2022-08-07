82820f2e06f4b24ae01cb4d736a8bfeb6ef5d04f
Shreyas3108T <85017158+Shreyas3108T@users.noreply.github.com>
2022-08-07 13:13:47 +0530

    routes defined

---
+++ routes/tickets.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+
+router.post("/users/new",
+/*middlewares*/
+ /*controller*/)
+
+router.post("/tickets/new"
+/*middlewares*/
+ /*controller*/)
+
+router,get("/ticket/:param"
+/*middlewares*/
+ /*controller*/)
+
+router.post("/tickets/markAsClosed"
+/*middlewares*/
+ /*controller*/)
+
+router.post("/tickets/delete"
+/*middlewares*/
+ /*controller*/)
+module.exports = router;
+
+// link https://fast-citadel-03015.herokuapp.com/
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config()
 
+const {routes} = require("./routes/tickets")
 
 const app = express()
 
 const port = process.env.PORT || 3000
-app.get("/",(req,res)=>{
-    res.send("test")
-})
+app.use("/",routes)
+
 app.listen(port, ()=>{
     console.log(`server Running at Port ${port}`)
 })
