a0060f16f83ba9ed9e587104b4a0949a13ba2d88
Shreyas3108T <85017158+Shreyas3108T@users.noreply.github.com>
2022-08-07 12:40:00 +0530

    heroku host test

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ require("dotenv").config()
 const app = express()
 
 const port = process.env.PORT || 3000
+app.get("/",(req,res)=>{
+    res.send("test")
+})
 app.listen(port, ()=>{
     console.log(`server Running at Port ${port}`)
 })
